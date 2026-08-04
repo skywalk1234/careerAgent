e02f795b0c6646125af2c3c3d6daf8d86478c197
毛睿 <16295475+skywalk7851@user.noreply.gitee.com>
2026-08-04 22:18:32 +0800

    不是写死，是 UserInfoInterceptor 在 resume-parser-service / profile-service 里根本没被注册 ：
    - 注册拦截器的 group.config.MvcConfig 在 common-service 里
    - 两个服务的 @SpringBootApplication 只扫描自己的包（ group.resumeparserservice / group.profileservice ），扫不到 group.config
    - 于是 UserContext.getUser() 恒为 null → FileController.java:62 兜底成 23
    - 对照：career-service 早就写了 @ComponentScan(basePackages = {"group.careerservice", "group.config", "group.interceptor", "group.utils"}) ，所以只有它没这毛病
    ## 修复
    按 career-service 的既有模式，给两个服务补上同样的扫描：
    
    - ResumeParserServiceApplication.java 加了 @ComponentScan(basePackages = {"group.resumeparserservice", "group.config", "group.interceptor", "group.utils"})
    - ProfileServiceApplication.java 同样加上

--- profile-service/src/main/java/group/profileservice/ProfileServiceApplication.java
+++ profile-service/src/main/java/group/profileservice/ProfileServiceApplication.java
@@ -2,8 +2,12 @@ package group.profileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// 必须扫描 common-service 的 group.config 才能注册 UserInfoInterceptor，
+// 否则 UserContext 恒为 null，获取画像时 userId 会兜底成 "111"
+@ComponentScan(basePackages = {"group.profileservice", "group.config", "group.interceptor", "group.utils"})
 public class ProfileServiceApplication {
 
     public static void main(String[] args) {
