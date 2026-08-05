41c96b7a2dba709f6ead06c84900b354334c7c26
毛睿 <16295475+skywalk7851@user.noreply.gitee.com>
2026-08-05 19:53:31 +0800

    修复Boot3.3下UserContext失效：新增jakarta版本地拦截器替代common-service的javax版

--- resume-parser-service/src/main/java/group/resumeparserservice/ResumeParserServiceApplication.java
+++ resume-parser-service/src/main/java/group/resumeparserservice/ResumeParserServiceApplication.java
@@ -5,7 +5,6 @@ import com.alibaba.cloud.ai.autoconfigure.dashscope.DashScopeAudioTranscriptionA
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @EnableFeignClients
@@ -15,9 +14,6 @@ import org.springframework.context.annotation.ComponentScan;
         // 排除语音转录/识别 (STT) - 解决你当前的报错
         DashScopeAudioTranscriptionAutoConfiguration.class
 })
-// 必须扫描 common-service 的 group.config 才能注册 UserInfoInterceptor，
-// 否则 UserContext 恒为 null，上传简历时 userId 会兜底成 23
-@ComponentScan(basePackages = {"group.resumeparserservice", "group.config", "group.interceptor", "group.utils"})
 public class ResumeParserServiceApplication {
 
     public static void main(String[] args) {
