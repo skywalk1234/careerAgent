d025f703b2e1fa7ba83418ea5ab650af639e6ccb
毛睿 <16295475+skywalk7851@user.noreply.gitee.com>
2026-09-11 08:59:19 +0800

    初始化单体架构

---
+++ src/main/java/group/career_backend/CareerBackendApplication.java
@@ -0,0 +1,13 @@
+package group.career_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/career_backend/CareerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group.career_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
